f68e73828a7adb80a5ae181d2be8c07363871356
rodion <rodotokma@gmail.com>
2025-11-12 14:35:31 +0300

    Добавлены java-doc для репозиториев

--- Task-1/src/main/java/ru/ylab/tasks/task1/repository/UserRepository.java
+++ Task-1/src/main/java/ru/ylab/tasks/task1/repository/UserRepository.java
@@ -5,9 +5,34 @@ import ru.ylab.tasks.task1.model.User;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Репозиторий для управления пользователями.
+ * Определяет операции для поиска, сохранения и проверки существования пользователей.
+ */
 public interface UserRepository {
+    /**
+     * Сохраняет нового пользователя или обновляет существующего.
+     * @param user пользователь для сохранения
+     */
     void save(User user);
+
+    /**
+     * Ищет пользователя по логину.
+     * @param login логин пользователя
+     * @return найденный пользователь (если существует)
+     */
     Optional<User> findByLogin(String login);
+
+    /**
+     * Возвращает список всех пользователей.
+     * @return список пользователей
+     */
     List<User> findAll();
+
+    /**
+     * Проверяет, существует ли пользователь с указанным логином.
+     * @param login логин для проверки
+     * @return {@code true}, если пользователь существует
+     */
     boolean existsByLogin(String login);
 }
