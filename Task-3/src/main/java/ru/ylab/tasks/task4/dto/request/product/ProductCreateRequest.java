300fe74c9755fbb895a7d12e659392a75a91f1e6
rodion <rodotokma@gmail.com>
2025-11-30 14:55:50 +0300

    Добавлены интеграционные тесты для rest-контроллеров с использованием Testcontainers.

--- Task-3/src/main/java/ru/ylab/tasks/task4/dto/request/product/ProductCreateRequest.java
+++ Task-3/src/main/java/ru/ylab/tasks/task4/dto/request/product/ProductCreateRequest.java
@@ -44,4 +44,24 @@ public class ProductCreateRequest {
     public String getDescription() {
         return description;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
